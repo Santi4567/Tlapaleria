807117f12ef0e99faee61d5998911c90ca910530
Santi4567 <santiantozac345@gmail.com>
2026-05-29 15:48:56 -0600

    IReturnService agregado

---
+++ Api Tlapaleria/Service/IReturnService.cs
@@ -0,0 +1,10 @@
+﻿using Api_Tlapaleria.DTOs;
+using Api_Tlapaleria.Models;
+
+namespace Api_Tlapaleria.Services
+{
+    public interface IReturnService
+    {
+        Task<PagedResponse<SaleReturn>> GetReturnsAsync(string? search = null, int pageNumber = 1, int pageSize = 50);
+    }
+}
